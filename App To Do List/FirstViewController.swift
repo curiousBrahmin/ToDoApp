f4e69cc2219b91cfaccc4ffdaad089c580724206
curiousBrahmin <Rambo@Rambos-iMac.hsd1.tn.comcast.net>
2016-09-17 19:18:52 -0500

    to do app

--- App To Do List/FirstViewController.swift
+++ App To Do List/FirstViewController.swift
@@ -8,18 +8,60 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var table: UITableView!
+    
+    var itemsArray: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+   
+       
+    }
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        let itemsObject = UserDefaults.standard.object(forKey: "items")
+        
+        if let tempItems = itemsObject as? [String]{
+            
+            itemsArray = tempItems
+        }
+        
+        table.reloadData()
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        
+        return itemsArray.count
     }
+    
 
-
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as UITableViewCell
+        
+              cell.textLabel?.text = itemsArray[indexPath.row]
+      
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        
+        if  editingStyle == UITableViewCellEditingStyle.delete{
+            
+            itemsArray.remove(at: indexPath.row)
+            
+            table.reloadData()
+            
+            UserDefaults.standard.set(itemsArray, forKey: "items")
+            
+        }
+    }
+    
+    
 }
 
--- App To Do List/SecondViewController.swift
+++ App To Do List/SecondViewController.swift
@@ -8,18 +8,49 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITextFieldDelegate{
+    
+    
+    @IBOutlet weak var textFieldInput: UITextField!
+    
+    @IBAction func addItem(_ sender: AnyObject) {
+        
+        let itemsObject = UserDefaults.standard.object(forKey: "items")
+        
+        var itemsArray: [String]
+        
+        if let tempItems = itemsObject as? [String]{
+            
+            itemsArray = tempItems
+            
+            itemsArray.append(textFieldInput.text!)
+            
+        }else{
+            
+            itemsArray = [textFieldInput.text!]
+        }
+        
+        UserDefaults.standard.set(itemsArray, forKey: "items")
+        
+        textFieldInput.text = ""
+  
+        
+    }
+
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
 
